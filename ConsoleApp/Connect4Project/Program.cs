a9f65095cb43ef6f6a112bec78fdda83241c5433
Bohdan Romashchenko <bohdan.romashchenko@gmail.com>
2019-09-30 15:03:17 +0300

    created first solution

---
+++ ConsoleApp/Connect4Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connect4Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
