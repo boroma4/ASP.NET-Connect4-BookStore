294e653d12c3f3edb3f432cda57db0ed5f75e987
Bohdan Romashchenko <bohdan.romashchenko@gmail.com>
2019-11-19 00:04:32 +0200

    Added basic pages

---
+++ BookStore/WebApp/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using Domain;
+
+namespace WebApp.Pages_Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DAL.AppDbContext _context;
+
+        public IndexModel(DAL.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Books
+                .Include(b => b.Language)
+                .Include(b => b.Publisher)
+                .Include(b=>b.BookAuthors)
+                .ThenInclude(b=> b.Author).ToListAsync();
+        }
+    }
+}
